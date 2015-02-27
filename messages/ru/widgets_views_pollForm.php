d3b640424bb9777dcc2975547f20649c1e63763e
Lucas Bartholemy <lucas@bartholemy.com>
2015-02-27 12:12:47 +0100

    Updated translations (translate.humhub.org)

--- messages/ru/widgets_views_pollForm.php
+++ messages/ru/widgets_views_pollForm.php
@@ -1,6 +1,6 @@
 <?php
 return array (
   'Allow multiple answers per user?' => 'Разрешить несколько варинатов ответов пользователю?',
-  'Ask something...' => 'Ответить как нибудь...',
-  'Possible answers (one per line)' => 'Возможные ответы (по одному в каждой строке) ',
+  'Ask something...' => 'Спросить как нибудь...',
+  'Possible answers (one per line)' => 'Варианты ответов (по одному в каждой строке) ',
 );
