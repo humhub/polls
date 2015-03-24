3eb9b5fe454fe3e7ffd6b1458e43d57db879b771
Lucas Bartholemy <lucas@bartholemy.com>
2015-03-24 20:24:10 +0100

    Updated translations (translate.humhub.org)

--- messages/fr/controllers_PollController.php
+++ messages/fr/controllers_PollController.php
@@ -4,5 +4,5 @@ return array (
   'Invalid answer!' => 'Réponse invalide !',
   'Users voted for: <strong>{answer}</strong>' => 'Les utilisateurs ont choisi : <strong>{answer}</strong>',
   'Voting for multiple answers is disabled!' => 'Réponse multiple impossible.',
-  'You have insufficient permissions to perform that operation!' => 'Vous n\'avez pas la permission de faire ça !',
+  'You have insufficient permissions to perform that operation!' => 'Vous n\'avez pas la permission de faire ça.',
 );
--- messages/fr/models_Poll.php
+++ messages/fr/models_Poll.php
@@ -2,6 +2,6 @@
 return array (
   'Answers' => 'Réponses',
   'Multiple answers per user' => 'Réponses multiples par utilisateur',
-  'Please specify at least {min} answers!' => 'Attention, vous devrez spécifier au minimum {min} réponses !',
+  'Please specify at least {min} answers!' => 'Attention, vous devrez spécifier {min} réponses minimum.',
   'Question' => 'Question',
 );
