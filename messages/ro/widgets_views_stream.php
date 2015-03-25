19ce72777dda220cf42c1162ba5d7c457138b7e2
Lucas Bartholemy <lucas@bartholemy.com>
2015-03-25 21:58:22 +0100

    Updated translations

--- messages/ro/widgets_views_stream.php
+++ messages/ro/widgets_views_stream.php
@@ -2,13 +2,13 @@
 return array (
   '<b>There are no polls yet!</b><br>Be the first and create one...' => '',
   'Asked by me' => '',
-  'Creation time' => 'Aanmaak datum',
+  'Creation time' => '',
   'Display all' => '',
-  'Filter' => 'Filter',
-  'Last update' => 'Laatste update',
+  'Filter' => '',
+  'Last update' => '',
   'No answered yet' => '',
   'No poll found which matches your current filter(s)!' => '',
   'Only private polls' => '',
   'Only public polls' => '',
-  'Sorting' => 'Sortering',
+  'Sorting' => '',
 );
