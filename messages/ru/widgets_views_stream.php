620172f81a073c42f9515f30914a6293f12e95df
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-13 10:35:52 +0200

    Updated translations (translate.humhub.org)

--- messages/ru/widgets_views_stream.php
+++ messages/ru/widgets_views_stream.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  '<b>There are no polls yet!</b>' => '',
-  'Display all' => '@@Показать все@@',
-  'No poll found which matches your current filter(s)!' => '@@Ответ соответствующий вашему фильтру не найден!@@',
+  '<b>There are no polls yet!</b>' => '<b>Здесь нет пока опросов!</b>',
   '<b>There are no polls yet!</b><br>Be the first and create one...' => '<b> Опросов пока нет! </ b> <br> Будьте первым...',
   'Asked by me' => 'Мои опросы',
   'Creation time' => 'Время создания',
+  'Display all' => '@@Показать все@@',
   'Filter' => 'Фильтр',
   'Last update' => 'Последнее обновление',
   'No answered yet' => 'Пока без ответа',
+  'No poll found which matches your current filter(s)!' => '@@Ответ соответствующий вашему фильтру не найден!@@',
   'Only private polls' => 'Только приватные опросы',
   'Only public polls' => 'Только публичные опросы',
   'Sorting' => 'Сортировка',
