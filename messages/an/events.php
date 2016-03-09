479ebbe8808d99e8fc507133dcff08174cb7faf6
HumHub Translate <info@humhub.com>
2016-03-09 21:09:01 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/an/events.php
+++ messages/an/events.php
@@ -18,10 +18,9 @@
  */
 return [
     'Again? ;Weary;' => '',
+    'Club A Steakhouse' => '',
+    'Pisillo Italian Panini' => '',
     'Right now, we are in the planning stages for our next meetup and we would like to know from you, where you would like to go?' => '',
-    'To Daniel
-Club A Steakhouse
-Pisillo Italian Panini
-' => '',
+    'To Daniel' => '',
     'Why don\'t we go to Bemelmans Bar?' => '',
 ];
