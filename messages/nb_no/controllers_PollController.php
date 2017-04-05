5bb2a7537ad19abe8fab77c94690b3fd2878ed6d
HumHub Translations <info@humhub.com>
2017-04-05 09:40:26 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/nb_no/controllers_PollController.php
+++ messages/nb_no/controllers_PollController.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'Access denied!' => 'Ingen adgang!',
-  'Anonymous poll!' => '',
-  'Could not load poll!' => '',
-  'Invalid answer!' => '',
-  'Users voted for: <strong>{answer}</strong>' => '',
-  'Voting for multiple answers is disabled!' => '',
-  'You have insufficient permissions to perform that operation!' => '',
+  'Access denied!' => 'Ingen tilgang!',
+  'Anonymous poll!' => 'Anonym meningsmåling',
+  'Could not load poll!' => 'Kunne ikke laste meningsmåling!',
+  'Invalid answer!' => 'Ugyldig svar!',
+  'Users voted for: <strong>{answer}</strong>' => 'Brukerne stemte for: <strong>{answer}</strong>',
+  'Voting for multiple answers is disabled!' => 'Stemming på flere alternativer er ikke tillatt!',
+  'You have insufficient permissions to perform that operation!' => 'Du har ikke tilgang til å utføre handlingen!',
 );
