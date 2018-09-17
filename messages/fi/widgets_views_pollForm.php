86f2586292406b9120fba90c46dd369b6d20f852
HumHub Translations <info@humhub.com>
2018-09-17 11:46:21 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/fi/widgets_views_pollForm.php
+++ messages/fi/widgets_views_pollForm.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Hide results until poll is closed?' => '',
-    'Add answer...' => 'Lisää vastaus...',
-    'Anonymous Votes?' => 'Haluatko että vastaukset ovat anonymeja?',
-    'Ask something...' => 'Kysy jotain...',
-    'Display answers in random order?' => 'Näytä vastaukset satunnaisessa järjestyksessä',
-    'Edit answer (empty answers will be removed)...' => 'Muokkaa vastausta (tyhäjä vastaus voidaan poistaa)...',
-    'Edit your poll question...' => 'Muokkaa sinun kyselyn kysymyksiä...',
-];
+return array (
+  'Add answer...' => 'Lisää vastaus...',
+  'Anonymous Votes?' => 'Haluatko että vastaukset ovat anonymeja?',
+  'Ask something...' => 'Kysy jotain...',
+  'Display answers in random order?' => 'Näytä vastaukset satunnaisessa järjestyksessä',
+  'Edit answer (empty answers will be removed)...' => 'Muokkaa vastausta (tyhäjä vastaus voidaan poistaa)...',
+  'Edit your poll question...' => 'Muokkaa sinun kyselyn kysymyksiä...',
+  'Hide results until poll is closed?' => 'Piilota tulokset, kunnes kysely on suljettu?',
+);
