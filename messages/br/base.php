024eaef25b30f2167598a086e2df9433d21aec38
HumHub Translations <info@humhub.com>
2019-03-26 19:03:56 +0100

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/br/base.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Allows to start polls.' => '',
+    'At least one answer is required' => '',
+    'Cancel' => '',
+    'Polls' => '',
+    'Save' => '',
+];
---
+++ messages/br/events.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Again? ;Weary;' => '',
+    'Club A Steakhouse' => '',
+    'Pisillo Italian Panini' => '',
+    'Right now, we are in the planning stages for our next meetup and we would like to know from you, where you would like to go?' => '',
+    'To Daniel' => '',
+    'Why don\'t we go to Bemelmans Bar?' => '',
+];
