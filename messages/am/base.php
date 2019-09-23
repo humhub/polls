d9c27e3b10425e6afe6b5071169eb4cee198929c
HumHub Translations <info@humhub.com>
2019-09-23 15:04:34 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/am/base.php
+++ messages/am/base.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'Allows to start polls.' => '',
-    'At least one answer is required' => '',
-    'Cancel' => '',
-    'Polls' => '',
-    'Save' => '',
-];
+return array (
+  'Allows to start polls.' => '',
+  'At least one answer is required' => '',
+  'Cancel' => 'ይቅር',
+  'Polls' => '',
+  'Save' => 'አስቀምጥ',
+);
--- messages/am/controllers_PollController.php
+++ messages/am/controllers_PollController.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Access denied!' => '',
-    'Anonymous poll!' => '',
-    'Could not load poll!' => '',
-    'Invalid answer!' => '',
-    'Users voted for: <strong>{answer}</strong>' => '',
-    'Voting for multiple answers is disabled!' => '',
-    'You have insufficient permissions to perform that operation!' => '',
-];
+return array (
+  'Access denied!' => 'ትዕዛዝዎ ተቀባይነት አላገኘም!',
+  'Anonymous poll!' => '',
+  'Could not load poll!' => '',
+  'Invalid answer!' => '',
+  'Users voted for: <strong>{answer}</strong>' => '',
+  'Voting for multiple answers is disabled!' => '',
+  'You have insufficient permissions to perform that operation!' => '',
+);
