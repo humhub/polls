577b8c4fd1a4ed842a009dfdeed3a5c5c1722465
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2019-11-07 10:12:48 -0500

    Enh: Implement Profile Polls

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 use humhub\modules\space\widgets\Menu;
 use humhub\modules\user\models\User;
+use humhub\modules\user\widgets\ProfileMenu;
 use humhub\commands\IntegrityController;
 use humhub\modules\content\widgets\WallEntryControls;
 
@@ -14,6 +15,7 @@ return [
         ['class' => WallEntryControls::class, 'event' => WallEntryControls::EVENT_INIT, 'callback' => ['humhub\modules\polls\Events', 'onWallEntryControlsInit']],
         ['class' => User::class, 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\polls\Events', 'onUserDelete']],
         ['class' => Menu::class, 'event' => Menu::EVENT_INIT, 'callback' => ['humhub\modules\polls\Events', 'onSpaceMenuInit']],
+        ['class' => ProfileMenu::class, 'event' => ProfileMenu::EVENT_INIT, 'callback' => ['humhub\modules\polls\Events', 'onProfileMenuInit']],
         ['class' => IntegrityController::class, 'event' => IntegrityController::EVENT_ON_RUN, 'callback' => ['humhub\modules\polls\Events', 'onIntegrityCheck']],
         ['class' => 'humhub\modules\installer\controllers\ConfigController', 'event' => 'install_sample_data', 'callback' => ['humhub\modules\polls\Events', 'onSampleDataInstall']],
     ]
