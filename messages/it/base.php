1ccca8df61f4c65279c6f2437e36478b2003e7ec
HumHub Translations <info@humhub.com>
2019-11-13 15:07:05 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/it/base.php
+++ messages/it/base.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'At least one answer is required' => '',
-    'Allows to start polls.' => 'Consente di iniziare un sondaggio.',
-    'Cancel' => 'Annulla',
-    'Polls' => 'Sondaggi',
-    'Save' => 'Salva',
-];
+return array (
+  'Allows to start polls.' => 'Consente di iniziare un sondaggio.',
+  'At least one answer is required' => 'E\' necessaria almeno ina risposta.',
+  'Cancel' => 'Annulla',
+  'Polls' => 'Sondaggi',
+  'Save' => 'Salva',
+);
