5ca39460b622728c3e7b8d507ec2115f58c3431c
hide-me <45032974+hide-me@users.noreply.github.com>
2020-04-26 20:22:01 +0300

    Russian translation

--- messages/ru/base.php
+++ messages/ru/base.php
@@ -1,8 +1,10 @@
 <?php
-
+/**
+ * Translation: Paul (https://paul.bid) paulbid@protonmail.com
+ */
 return [
-    'Allows to start polls.' => '',
-    'At least one answer is required' => '',
+    'Allows to start polls.' => 'Разрешить создавать опросы',
+    'At least one answer is required' => 'Требуется хотя бы один ответ',
     'Cancel' => 'Отменить',
     'Polls' => 'Опросы',
     'Save' => 'Сохранить',
