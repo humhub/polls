9006058e346f967223d489befc66fd766b4133a2
HumHub Translations <info@humhub.com>
2020-06-29 15:13:07 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,9 +1,8 @@
 <?php
-
-return [
-    'At least one answer is required' => '',
-    'Allows to start polls.' => 'Permite iniciar encuestas.',
-    'Cancel' => 'Cancelar',
-    'Polls' => 'Votaciones',
-    'Save' => 'Guardar',
-];
+return array (
+  'Allows to start polls.' => 'Permite iniciar encuestas.',
+  'At least one answer is required' => 'Se requiere al menos una respuesta',
+  'Cancel' => 'Cancelar',
+  'Polls' => 'Votaciones',
+  'Save' => 'Guardar',
+);
