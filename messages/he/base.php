9f5fc473fa4f4adfa3494826dde1b3102dd3fd4d
HumHub Translations <info@humhub.com>
2020-09-04 10:44:26 +0200

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/he/base.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'Allows to start polls.' => '',
+  'At least one answer is required' => '',
+  'Cancel' => 'ביטול',
+  'Polls' => '',
+  'Save' => 'שמירה',
+);
---
+++ messages/he/widgets_views_pollForm.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Add answer...' => '',
+    'Anonymous Votes?' => '',
+    'Ask something...' => '',
+    'Display answers in random order?' => '',
+    'Edit answer (empty answers will be removed)...' => '',
+    'Edit your poll question...' => '',
+    'Hide results until poll is closed?' => '',
+];
