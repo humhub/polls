885cf07c5cd5b6daf66e250f6fee10c56f437a03
yurabakhtin <yurybakh@gmail.com>
2020-10-19 13:44:45 +0300

    Wall Stream Layout Migration

--- widgets/WallEntry.php
+++ widgets/WallEntry.php
@@ -2,6 +2,7 @@
 
 namespace humhub\modules\polls\widgets;
 
+use humhub\modules\content\widgets\stream\WallStreamModuleEntryWidget;
 use Yii;
 
 /**
@@ -13,21 +14,28 @@ use Yii;
  * @since 0.5
  * @author Luke
  */
-class WallEntry extends \humhub\modules\content\widgets\WallEntry
+class WallEntry extends WallStreamModuleEntryWidget
 {
 
     public $editRoute = "/polls/poll/edit";
     
-    public function run()
+    public function renderContent()
     {
         //We don't want an edit menu when the poll is closed
-        if(version_compare(Yii::$app->version, '1.0.0-beta.4', 'lt') || $this->contentObject->closed) {
+        if(version_compare(Yii::$app->version, '1.0.0-beta.4', 'lt') || $this->model->closed) {
             $this->editRoute = '';
         }
 
-        return $this->render('entry', ['poll' => $this->contentObject,
-                    'user' => $this->contentObject->content->createdBy,
-                    'contentContainer' => $this->contentObject->content->container]);
+        return $this->render('entry', ['poll' => $this->model,
+                    'user' => $this->model->content->createdBy,
+                    'contentContainer' => $this->model->content->container]);
     }
 
+    /**
+     * @return string a non encoded plain text title (no html allowed) used in the header of the widget
+     */
+    protected function getTitle()
+    {
+        return $this->model->question;
+    }
 }
