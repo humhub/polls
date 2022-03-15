c8e89269db838199972f2ef08f1880d5f3b10eb2
HumHub Translations <info@humhub.com>
2022-03-15 23:44:34 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/sv/events.php
+++ messages/sv/events.php
@@ -1,11 +1,10 @@
 <?php
-
-return [
-    'Again? ;Weary;' => '',
-    'Club A Steakhouse' => '',
-    'Location of the next meeting' => '',
-    'Pisillo Italian Panini' => '',
-    'Right now, we are in the planning stages for our next meetup and we would like to know from you, where you would like to go?' => '',
-    'To Daniel' => '',
-    'Why don\'t we go to Bemelmans Bar?' => '',
-];
+return array (
+  'Again? ;Weary;' => 'Om igen? ;Trötta;',
+  'Club A Steakhouse' => 'Club A Steakhouse',
+  'Location of the next meeting' => 'Plats för nästa möte',
+  'Pisillo Italian Panini' => 'Pisillo italienska Panini',
+  'Right now, we are in the planning stages for our next meetup and we would like to know from you, where you would like to go?' => 'Just nu är vi i planeringsstadiet för vår nästa träff och vi skulle vilja veta vart du skulle vilja åka?',
+  'To Daniel' => 'Till Daniel',
+  'Why don\'t we go to Bemelmans Bar?' => 'Varför går vi inte till Bemelmans Bar?',
+);
