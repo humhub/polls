b4bc33e7813ea71b349917cb086056187940e5f1
HumHub Translations <hello@humhub.com>
2022-06-20 16:45:14 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/zh-TW/base.php
+++ messages/zh-TW/base.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Save' => '儲存',
-    'Allows the user to create polls' => '',
-    'Allows to start polls.' => '',
-    'Answers' => '',
-    'At least one answer is required' => '',
-    'Cancel' => '',
-    'Create poll' => '',
-    'Polls' => '',
-];
+return array (
+  'Allows the user to create polls' => '',
+  'Allows to start polls.' => '',
+  'Answers' => '',
+  'At least one answer is required' => '',
+  'Cancel' => '取消',
+  'Create poll' => '',
+  'Polls' => '',
+  'Save' => '儲存',
+);
