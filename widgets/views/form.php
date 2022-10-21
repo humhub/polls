08b5797f2b9108c0183a84d9ce1c416c0ae841b3
Yuriy Bakhtin <yurybakh@gmail.com>
2022-10-21 19:04:44 +0400

    Improve styles on create poll form (for case with error messages)

--- widgets/views/form.php
+++ widgets/views/form.php
@@ -20,10 +20,10 @@ PollsAsset::register($this);
 <div class="contentForm_options" data-content-component="polls.Poll">
     <?= $form->field($model, 'description')->widget(RichTextField::class, [
         'placeholder' => Yii::t('PollsModule.widgets_views_pollForm', 'Description'),
-        'options' => ['style' => 'margin:20px 0 15px'],
+        'options' => ['style' => 'margin:20px 0 0'],
     ])->label(false) ?>
 
-    <?= Html::activeLabel($model, 'answersText', ['label' => Yii::t('PollsModule.base', 'Answers'), 'class' => 'control-label']); ?>
+    <?= Html::activeLabel($model, 'answersText', ['label' => Yii::t('PollsModule.base', 'Answers'), 'class' => 'control-label', 'style' => 'margin-top:15px']); ?>
     <?= AddAnswerInput::widget(['name' => 'newAnswers[]', 'showTitle' => false]); ?>
 
     <div class="row">
