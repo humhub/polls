4ed00d6af6596c8dde03794a5e0920dff03f06d1
HumHub Translations <hello@humhub.com>
2022-11-10 19:28:10 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/sw/widgets_views_pollForm.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Add answer...' => '',
+    'Anonymous Votes?' => '',
+    'Description' => '',
+    'Display answers in random order?' => '',
+    'Edit answer (empty answers will be removed)...' => '',
+    'Edit your poll question...' => '',
+    'Hide results until poll is closed?' => '',
+    'Question' => '',
+];
