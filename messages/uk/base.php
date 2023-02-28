06bbece5d978cc0111dd42bade433c2ba42cc505
HumHub Translations <hello@humhub.com>
2023-02-28 07:38:14 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/uk/base.php
+++ messages/uk/base.php
@@ -1,12 +1,11 @@
 <?php
-
-return [
-    'Allows the user to create polls' => '',
-    'Allows to start polls.' => '',
-    'Answers' => '',
-    'At least one answer is required' => '',
-    'Cancel' => '',
-    'Create poll' => '',
-    'Polls' => '',
-    'Save' => '',
-];
+return array (
+  'Allows the user to create polls' => '',
+  'Allows to start polls.' => '',
+  'Answers' => '',
+  'At least one answer is required' => '',
+  'Cancel' => 'Скасувати',
+  'Create poll' => '',
+  'Polls' => '',
+  'Save' => 'Зберегти',
+);
