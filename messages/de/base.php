04a05d25e85b8a874072c98353effa94ab5ee640
HumHub Translations <hello@humhub.com>
2023-03-10 14:55:03 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/base.php
+++ messages/de/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Allows the user to create polls' => 'Erlaubt es dem Benutzer Umfragen zu erstellen',
-    'Allows to start polls.' => 'Ermöglicht Umfragen zu starten.',
-    'Answers' => 'Antworten',
-    'At least one answer is required' => 'Mindestens eine Antwort erforderlich.',
-    'Cancel' => 'Abbrechen',
-    'Create poll' => 'Umfrage erstellen',
-    'Polls' => 'Umfragen',
-    'Save' => 'Speichern',
-    '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '',
-];
+return array (
+  'Allows the user to create polls' => 'Erlaubt es dem Benutzer Umfragen zu erstellen',
+  'Allows to start polls.' => 'Ermöglicht Umfragen zu starten.',
+  'Answers' => 'Antworten',
+  'At least one answer is required' => 'Mindestens eine Antwort erforderlich.',
+  'Cancel' => 'Abbrechen',
+  'Create poll' => 'Umfrage erstellen',
+  'Polls' => 'Umfragen',
+  'Save' => 'Speichern',
+  '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '{n,plural,=1{# {htmlTagBegin}Umfrage{htmlTagEnd}}other{# {htmlTagBegin}Umfragen{htmlTagEnd}}}',
+);
--- messages/nl/base.php
+++ messages/nl/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Allows the user to create polls' => 'Sta de gebruiker toe stembussen maken',
-    'Allows to start polls.' => 'Starten van stembussen toestaan.',
-    'Answers' => 'Antwoorden',
-    'At least one answer is required' => 'Ten minste één antwoord is vereist',
-    'Cancel' => 'Annuleer',
-    'Create poll' => 'Maak een stembus',
-    'Polls' => 'Stembus',
-    'Save' => 'Bewaar',
-    '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '',
-];
+return array (
+  'Allows the user to create polls' => 'Sta de gebruiker toe stembussen maken',
+  'Allows to start polls.' => 'Starten van stembussen toestaan.',
+  'Answers' => 'Antwoorden',
+  'At least one answer is required' => 'Ten minste één antwoord is vereist',
+  'Cancel' => 'Annuleer',
+  'Create poll' => 'Maak een stembus',
+  'Polls' => 'Stembus',
+  'Save' => 'Bewaar',
+  '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '{n,plural,=1{# {htmlTagBegin}stem{htmlTagEnd}}other{# {htmlTagBegin}stemmen{htmlTagEnd}}}',
+);
