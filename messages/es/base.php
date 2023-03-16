bc272674cdc9b20e71f51f02151ab13c82b77fc4
HumHub Translations <hello@humhub.com>
2023-03-16 19:39:49 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Allows the user to create polls' => 'Permite al usuario crear encuestas.',
-    'Allows to start polls.' => 'Permite iniciar encuestas.',
-    'Answers' => 'Respuestas',
-    'At least one answer is required' => 'Se requiere al menos una respuesta',
-    'Cancel' => 'Cancelar',
-    'Create poll' => 'Crear encuesta',
-    'Polls' => 'Votaciones',
-    'Save' => 'Guardar',
-    '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '',
-];
+return array (
+  'Allows the user to create polls' => 'Permite al usuario crear encuestas.',
+  'Allows to start polls.' => 'Permite iniciar encuestas.',
+  'Answers' => 'Respuestas',
+  'At least one answer is required' => 'Se requiere al menos una respuesta',
+  'Cancel' => 'Cancelar',
+  'Create poll' => 'Crear encuesta',
+  'Polls' => 'Votaciones',
+  'Save' => 'Guardar',
+  '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}' => '{n,plural,=1{# {htmlTagBegin}vote{htmlTagEnd}}other{# {htmlTagBegin}votes{htmlTagEnd}}}',
+);
