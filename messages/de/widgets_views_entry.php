890200e9b0491406e4e888c669764c885aeda0b0
HumHub Translations <hello@humhub.com>
2023-06-29 14:29:56 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/widgets_views_entry.php
+++ messages/de/widgets_views_entry.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  '<strong>Note:</strong> The result is hidden until the poll is closed by a moderator.' => '<strong>Hinweis:</strong> Das Ergebnis wir erst nach dem Schließen der Umfrage angezeigt.',
+  '<strong>Note:</strong> The result is hidden until the poll is closed by a moderator.' => '<strong>Hinweis:</strong> Das Ergebnis wird erst nach Beendigung der Umfrage angezeigt.',
   'Anonymous' => 'Anonym',
   'Closed' => 'Beendet',
   'Complete Poll' => 'Umfrage schließen',
