8fe91f1d8c4a0abbb725a2a5b5098662819244e4
HumHub Translations <hello@humhub.com>
2023-10-10 07:10:57 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/eu/controllers_PollController.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Access denied!' => '',
+    'Anonymous poll!' => '',
+    'Could not load poll!' => '',
+    'Invalid answer!' => '',
+    'Users voted for: <strong>{answer}</strong>' => '',
+    'Voting for multiple answers is disabled!' => '',
+    'You have insufficient permissions to perform that operation!' => '',
+];
