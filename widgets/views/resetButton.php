fbe796b29ae180d5ebd370f581db2f24548a6223
Felix Hahn <info@hahn-felix.de>
2025-04-17 21:40:24 +0200

    add dropdown-item

--- widgets/views/resetButton.php
+++ widgets/views/resetButton.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="#" data-action-click="reset"
+    <a class="dropdown-item" href="#" data-action-click="reset"
        data-action-target="[data-poll='<?= $poll->id ?>']" 
        data-action-url="<?= $poll->content->container->createUrl('/polls/poll/answer-reset', ['pollId' => $poll->id]); ?>">
         <i class="fa fa-undo"></i>
